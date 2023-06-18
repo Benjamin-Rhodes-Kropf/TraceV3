0fa6582b043004c2d0449826cbf6c8ea87c1ccb1
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2023-06-17 20:06:04 -0400

    Added HapticManager and Haptics

---
+++ TraceV3/Assets/Scripts/Managers/HapticManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.NiceVibrations;
+using UnityEngine;
+
+public class HapticManager : MonoBehaviour
+{
+    public void CameraHaptic()
+    {
+        MMVibrationManager.Haptic(HapticTypes.SoftImpact); //Todo: on input on release button
+    }
+    public void SelectionHaptic()
+    {
+        MMVibrationManager.Haptic(HapticTypes.LightImpact);
+    }
+}
