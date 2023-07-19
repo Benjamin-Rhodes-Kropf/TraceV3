91f694d0f15482d5b5de48885a66ab8ef04799ec
Salman Saleem <salman.saleem.dev@gmail.com>
2023-07-19 15:06:01 +0500

    -- Search Functionality Implemented

--- TraceV3/Assets/Scripts/CanvasManagers/SelectFriends/SelectFriendsControler.cs
+++ TraceV3/Assets/Scripts/CanvasManagers/SelectFriends/SelectFriendsControler.cs
@@ -14,11 +14,13 @@ public class SelectFriendsControler : MonoBehaviour
     {
         this._view = view;
         _allFriendsView = new List<SendToFriendView>();
+        this._view._searchBar.onValueChanged.AddListener(OnSearchBarValueChange);
         LoadAllFriends();
     }
     public void UnInitialize()
     {
         _allFriendsView.Clear();
+        this._view._searchBar.onValueChanged.RemoveAllListeners();
         Debug.Log("SelectFriendsControler: UnInitialize()");
     }
     
@@ -66,6 +68,7 @@ public class SelectFriendsControler : MonoBehaviour
             return;
         foreach (var view in _allFriendsView)
             GameObject.Destroy(view.gameObject);
+        _allFriendsView.Clear();
     }
     
     public void UpdateFriendsLayout()
@@ -85,6 +88,27 @@ public class SelectFriendsControler : MonoBehaviour
                 Debug.Log("UpdateFriendsSendTo:" + view.friendUID);
                 SendTraceManager.instance.usersToSendTo.Add(view.friendUID);
             }
+        }
+    }
+
+
+
+    private void OnSearchBarValueChange(string inputText)
+    {
+        ClearFriendsView();
+        if (inputText.Length <= 1)
+        {
+            LoadAllFriends();
+            return;
+        }
+        
+        Debug.Log("Search Friends");
+        int numOfBestFriends = 0;
+        var users = UserDataManager.Instance.GetFriendsByName(inputText);
+        foreach (var user in users)
+        {
+            bool isBestFriend = FriendsModelManager.Instance.IsBestFriend(user.userId);
+            UpdateFriendViewInfo(user, isBestFriend);
         }
     }
 }
