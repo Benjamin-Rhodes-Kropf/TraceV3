c0ec8f4a5bd0871ca8582c1f088c0ed56c7a33a2
Salman Saleem <salman.saleem.dev@gmail.com>
2023-07-24 18:39:43 +0500

    -- Contact Image Issue Resolved -- Search Bar Lag Issue Resolved

--- TraceV3/Assets/Scripts/CanvasManagers/Contact Canvas/ContactView.cs
+++ TraceV3/Assets/Scripts/CanvasManagers/Contact Canvas/ContactView.cs
@@ -44,7 +44,7 @@ public class ContactView : MonoBehaviour
          {
             var sprite = Sprite.Create(
                texture,
-               new Rect(0, 0, texture.width/2, texture.height/2),
+               new Rect(0, 0, texture.width, texture.height),
                new Vector2(0.5f, 0.5f));
             _contactImage.sprite = sprite;
             //_contactImage.sprite = CropTexture(texture);
